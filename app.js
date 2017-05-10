d109b504133c815ca0c793bbe7632399b55f5c96
chen-fang-1992 <fc812168079@gmail.com>
2017-05-11 01:30:35 +1000

    404 error page

--- app.js
+++ app.js
@@ -79,7 +79,11 @@ app.use(function(err, req, res, next) {
 
 	// render the error page
 	res.status(err.status || 500);
-	res.render('error');
+	res.locals.failMsg = 'Sorry, the page you are looking for is not exist.';
+	res.render('error', {
+		title: 'Error',
+		navbar: [{hp:'',as:'',cu:'',mp:'',sc:'',ru:'',ul:'',am:''}]
+	});
 });
 
 module.exports = app;
